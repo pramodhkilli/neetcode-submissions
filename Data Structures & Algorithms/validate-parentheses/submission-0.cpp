a3440e27bbc5c52f1415ebcabf564aff495e2ee9
pramodhkilli <71769013+pramodhkilli@users.noreply.github.com>
2026-09-02 01:03:41 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto &it:s){
+            if(it=='(' || it == '{' || it == '['){
+                st.push(it);
+            }else{
+                if(st.size()==0){
+                    return false;
+                }
+                if(it == ')' && st.top()!='('){
+                    return false;
+                }
+                if(it == '}' && st.top()!='{'){
+                    return false;
+                }
+                if(it == ']' && st.top()!='['){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.size() == 0;
+    }
+};
